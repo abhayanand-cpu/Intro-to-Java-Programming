7f98f179f484f6953652785e3bd000658e03993b
Abhayanand Siddharth <72178695+abhayanand-cpu@users.noreply.github.com>
2022-03-30 09:51:15 +0530

    Update Exercise_07_15.java

--- Exercise_07/Exercise_07_15/Exercise_07_15.java
+++ Exercise_07/Exercise_07_15/Exercise_07_15.java
@@ -5,50 +5,85 @@
 * Write a test program that reads in ten integers, invokes the method, and       *
 * displays the result.                                                           *
 *********************************************************************************/
+// import java.util.Scanner;
+
+// public class Exercise_07_15 {
+// 	/** Main method */
+// 	public static void main(String[] args) {
+// 		Scanner input = new Scanner(System.in); // Create a Scanner
+// 		int[] numbers = new int[10]; // Create an array of length 10
+
+// 		// Prompt the user to enter ten numbers
+// 		System.out.print("Enter ten number: ");
+// 		for (int i = 0; i < numbers.length; i++)
+// 			numbers[i] = input.nextInt();
+
+// 		// Get distinct numbers
+// 		int[] distinctNumbers = eliminateDuplicates(numbers);
+
+// 		// Display the result
+// 		System.out.print("The distinct numbers are:");
+// 		for (int e: distinctNumbers) {
+// 			if (e > 0)
+// 				System.out.print(" " + e);
+// 		}
+// 		System.out.println();
+// 	}
+
+// 	/** eleminateDuplicates returns a new array with duplicate values eliminated */
+// 	public static int[] eliminateDuplicates(int[] list) {
+// 		int[] distinctList = new int[list.length];
+// 		int i = 0;	// index distinctList
+// 		for (int e: list) {
+// 			if (linearSearch(distinctList, e) == -1) {
+// 				distinctList[i] = e;
+// 				i++;
+// 			}
+// 		}
+// 		return distinctList;
+// 	}
+
+// 	/** linearSearch */
+// 	public static int linearSearch(int[] array, int key) {
+// 		for (int i = 0; i < array.length; i++) {
+// 			if (key == array[i])
+// 				return i;
+// 		}
+// 		return -1;
+// 	}
+// }
+import java.util.Arrays;
 import java.util.Scanner;
+public class A8Q10 {
 
-public class Exercise_07_15 {
-	/** Main method */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in); // Create a Scanner
-		int[] numbers = new int[10]; // Create an array of length 10
-
-		// Prompt the user to enter ten numbers
-		System.out.print("Enter ten number: ");
-		for (int i = 0; i < numbers.length; i++)
-			numbers[i] = input.nextInt();
-
-		// Get distinct numbers
-		int[] distinctNumbers = eliminateDuplicates(numbers);
-
-		// Display the result
-		System.out.print("The distinct numbers are:");
-		for (int e: distinctNumbers) {
-			if (e > 0)
-				System.out.print(" " + e);
-		}
-		System.out.println();
-	}
-
-	/** eleminateDuplicates returns a new array with duplicate values eliminated */
 	public static int[] eliminateDuplicates(int[] list) {
-		int[] distinctList = new int[list.length];
-		int i = 0;	// index distinctList
-		for (int e: list) {
-			if (linearSearch(distinctList, e) == -1) {
-				distinctList[i] = e;
-				i++;
+		int arr[] = new int[10];
+		
+		for( int i = 0; i < list.length; i++ ) {
+			int count = 0;
+			for( int j = i-1; j >= 0; j-- ) {
+				if( list[j] == list[i] ) {
+					count++;
+					break;
+				}
 			}
+			if( count == 0 )
+				arr[i] = list[i];
 		}
-		return distinctList;
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n [] = new int[10];
+		
+		for( int i = 0; i < n.length; i++ ) {
+			int element = sc.nextInt();
+			n[i] = element;
+		}
+		int arr[] = eliminateDuplicates(n);
+		System.out.println(Arrays.toString(arr));
+		sc.close();
 	}
 
-	/** linearSearch */
-	public static int linearSearch(int[] array, int key) {
-		for (int i = 0; i < array.length; i++) {
-			if (key == array[i])
-				return i;
-		}
-		return -1;
-	}
 }
